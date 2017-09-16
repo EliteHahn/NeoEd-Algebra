daf838d913a6931d97f26ee8ad823667ac20e35b
Peter Hahn Linux <peterkunho@gmail.com>
2017-09-16 13:18:29 -0400

    FileMaker made

---
+++ FileMaker/src/FileMaker/FileMaker.scala
@@ -0,0 +1,25 @@
+package FileMaker
+
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+
+
+
+class FileMaker(file: File, name: String){
+
+  var fileReader = new FileReader(file)
+  var fileWriter = new FileWriter(new File(name + ".neo"))
+  var linked = new Fucak()
+
+  f = x.fun
+
+
+}
+
+object FileMaker{
+
+  def main (args: Array[String]): Unit = {
+
+  }
+}
